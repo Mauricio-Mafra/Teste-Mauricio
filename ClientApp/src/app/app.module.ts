d7fbb8ddb8397558f7dcbdbb7d9152226a771f2e
Mauricio-Mafra <mauricio.mafrapedreiro@gmail.com>
2022-03-15 07:09:27 -0300

    Route "/cadastro" added
    Initial "/cadastro" route added

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { CadastroComponent } from './cadastro/cadastro.component'
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'cadastro', component: CadastroComponent },
     ])
   ],
   providers: [],
---
+++ ClientApp/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,10 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-cadastro-component',
+  templateUrl: './cadastro.component.html'
+})
+export class CadastroComponent {
+
+}
